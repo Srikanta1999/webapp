69b9eaae59b778af88f6344f848371d9e0e97735
Lenovo <Lenovo@DESKTOP-2P52UUT>
2024-08-06 22:10:56 +0530

    done

---
+++ Hello123/src/main/java/com/example/demo/Hello123Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello123Application.class, args);
+	}
+
+}
---
+++ Hello123/src/test/java/com/example/demo/Hello123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
